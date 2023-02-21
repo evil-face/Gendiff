e310c2ba4a5d2955e4b084964e1906db38e83096
evil_face <den.bragin@gmail.com>
2023-02-21 21:53:00 +0600

    update Differ.java with flat JSON parsing

---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,56 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Differ {
+
+    public static String generate(String filepath1, String filepath2) throws IOException {
+
+        Path path1 = Paths.get(filepath1).normalize().toAbsolutePath();
+        Path path2 = Paths.get(filepath2).normalize().toAbsolutePath();
+
+        if (!Files.exists(path1)) {
+            throw new NoSuchFileException("File " + path1 + " does not exist");
+        }
+
+        if (!Files.exists(path2)) {
+            throw new NoSuchFileException("File " + path2 + " does not exist");
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> map1 = mapper.readValue(new File(path1.toString()), new TypeReference<>() {});
+        Map<String, String> map2 = mapper.readValue(new File(path2.toString()), new TypeReference<>() {});
+
+        Set<String> allKeysSet = new TreeSet<>();
+        allKeysSet.addAll(map1.keySet());
+        allKeysSet.addAll(map2.keySet());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{\n");
+        for (String key : allKeysSet) {
+            String value1 = map1.get(key);
+            String value2 = map2.get(key);
+            if (Objects.equals(value1, value2)) {
+                sb.append("    " + key + ": " + value1 + "\n");
+            } else if (value2 == null) {
+                sb.append("  - " + key + ": " + value1 + "\n");
+            } else if (value1 == null) {
+                sb.append("  + " + key + ": " + value2 + "\n");
+            } else {
+                sb.append("  - " + key + ": " + value1 + "\n");
+                sb.append("  + " + key + ": " + value2 + "\n");
+            }
+        }
+        sb.append("}");
+
+        return sb.toString();
+    }
+}
