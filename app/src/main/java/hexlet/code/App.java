5301762da8eeab28756df7db8443228f269ef3bc
evil_face <den.bragin@gmail.com>
2023-02-21 21:47:01 +0600

    update App.java with CLI parameters support

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,15 +5,30 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    String filepath2;
+
     @Override
     public Integer call() {
-        System.out.println("Hello World!");
+        try {
+            System.out.println(Differ.generate(filepath1, filepath2));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Try again");
+        }
         return 0;
     }
     public static void main(String[] args) {
