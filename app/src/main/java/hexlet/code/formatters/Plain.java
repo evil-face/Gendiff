507a1b6acc3f725d7a516c72a5f8268327604a2c
evil_face <den.bragin@gmail.com>
2023-02-28 14:34:17 +0600

    add formatter Plain.java to handle 'plain' option

---
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -0,0 +1,51 @@
+package hexlet.code.formatters;
+
+import hexlet.code.Node;
+import java.util.List;
+
+public class Plain {
+    public static String generate(List<Node> diffList) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Node node : diffList) {
+            switch (node.getStatus()) {
+                case "changed" -> {
+                    sb.append("Property '");
+                    sb.append(node.getKey());
+                    sb.append("' was updated. From ");
+                    sb.append(parseValue(node.getOldValue()));
+                    sb.append(" to ");
+                    sb.append(parseValue(node.getNewValue()));
+                    sb.append("\n");
+                }
+                case "removed" -> {
+                    sb.append("Property '");
+                    sb.append(node.getKey());
+                    sb.append("' was removed\n");
+                }
+                case "added" -> {
+                    sb.append("Property '");
+                    sb.append(node.getKey());
+                    sb.append("' was added with value: ");
+                    sb.append(parseValue(node.getNewValue()));
+                    sb.append("\n");
+                }
+                default -> sb.append("");
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String parseValue(Object value) {
+        String parsedValue;
+        if (value == null) {
+            parsedValue = "null";
+        } else if (value instanceof String) {
+            parsedValue = "'" + value + "'";
+        } else {
+            parsedValue = value.toString().startsWith("{") || value.toString().startsWith("[")
+                    ? "[complex value]" : value.toString();
+        }
+        return parsedValue;
+    }
+}
