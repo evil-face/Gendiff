ee85deacf478573087779ebdf32166c4b860ce59
evil_face <den.bragin@gmail.com>
2023-02-28 14:31:15 +0600

    update Differ.java with call to new Formatter.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -11,28 +11,16 @@ import java.util.TreeSet;
 
 public class Differ {
 
-    public static String generate(String filepath1, String filepath2) throws IOException {
+    public static String generate(String filepath1, String filepath2, String format) throws IOException {
         Map<String, Object> map1 = Parser.parse(filepath1);
         Map<String, Object> map2 = Parser.parse(filepath2);
         List<Node> diffList = buildDiffList(map1, map2);
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("{\n");
-        for (Node node : diffList) {
-            switch (node.getStatus()) {
-                case "unchanged" -> sb.append("    " + node.getKey() + ": " + node.getOldValue() + "\n");
-                case "removed" -> sb.append("  - " + node.getKey() + ": " + node.getOldValue() + "\n");
-                case "added" -> sb.append("  + " + node.getKey() + ": " + node.getNewValue() + "\n");
-                case "changed" -> {
-                    sb.append("  - " + node.getKey() + ": " + node.getOldValue() + "\n");
-                    sb.append("  + " + node.getKey() + ": " + node.getNewValue() + "\n");
-                }
-                default -> throw new IOException();
-            }
-        }
-        sb.append("}");
+        return Formatter.format(diffList, format);
+    }
 
-        return sb.toString();
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        return generate(filepath1, filepath2, "stylish");
     }
 
     private static List<Node> buildDiffList(Map<String, Object> map1, Map<String, Object> map2) {
