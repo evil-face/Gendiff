58355cd38c0bf22abead26712ea76e9d2d34d5e7
evil_face <den.bragin@gmail.com>
2023-02-28 23:41:07 +0600

    fix Node.java with linter tips

--- app/src/main/java/hexlet/code/Node.java
+++ app/src/main/java/hexlet/code/Node.java
@@ -1,16 +1,16 @@
 package hexlet.code;
 
-public class Node {
+public final class Node {
     private final String key;
     private final Object oldValue;
     private final Object newValue;
     private final String status;
 
-    public Node(String key, Object oldValue, Object newValue, String status) {
-        this.key = key;
-        this.oldValue = oldValue;
-        this.newValue = newValue;
-        this.status = status;
+    public Node(String diffKey, Object oldVal, Object newVal, String diffStatus) {
+        this.key = diffKey;
+        this.oldValue = oldVal;
+        this.newValue = newVal;
+        this.status = diffStatus;
     }
 
     public String getKey() {
