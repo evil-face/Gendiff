173671bf936dac022237cb2fc723de56f9f25b9d
evil_face <den.bragin@gmail.com>
2023-02-28 23:40:28 +0600

    fix App.java with linter tips

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,17 +10,17 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
 
     @Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish",
             description = "output format [default: stylish]")
-    String format;
+    private String format;
 
     @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
-    String filepath1;
+    private String filepath1;
 
     @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
-    String filepath2;
+    private String filepath2;
 
     @Override
     public Integer call() {
