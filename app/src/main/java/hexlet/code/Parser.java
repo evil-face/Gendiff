9185f3a9db6ca7a0f63519150c818109fd2759bf
evil_face <den.bragin@gmail.com>
2023-03-07 21:35:19 +0600

    refactor Parser.java to have extension constants

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -14,6 +14,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
+
+    public static final String JSON_EXT = "json";
+    public static final String YAML_EXT_1 = "yml";
+    public static final String YAML_EXT_2 = "yaml";
+
     public static Map<String, Object> parse(String filepath) throws IOException {
         Path path = Paths.get(filepath).normalize().toAbsolutePath();
         if (!Files.exists(path)) {
@@ -33,14 +38,14 @@ public class Parser {
         return parsedMap;
     }
 
-    private static ObjectMapper createMapper(String ext) throws IOException {
+    private static ObjectMapper createMapper(String ext) {
         ObjectMapper mapper;
-        if (ext.equalsIgnoreCase("json")) {
+        if (ext.equalsIgnoreCase(JSON_EXT)) {
             mapper = new ObjectMapper();
-        } else if (ext.equalsIgnoreCase("yml") || ext.equalsIgnoreCase("yaml")) {
+        } else if (ext.equalsIgnoreCase(YAML_EXT_1) || ext.equalsIgnoreCase(YAML_EXT_2)) {
             mapper = new YAMLMapper();
         } else {
-            throw new IOException("Not supported extension");
+            throw new RuntimeException("Not supported extension");
         }
         return mapper;
     }
