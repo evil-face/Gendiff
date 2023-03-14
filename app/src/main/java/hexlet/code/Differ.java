a51fae22b5a4e6114ce018079924acda9e547cdf
evil_face <den.bragin@gmail.com>
2023-03-14 15:51:45 +0600

    refactor Differ.java with extracted from Parser.java methods

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,24 +1,20 @@
 package hexlet.code;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
 
 
 public class Differ {
-    public static final String STATUS_ADDED = "added";
-    public static final String STATUS_REMOVED = "removed";
-    public static final String STATUS_UNCHANGED = "unchanged";
-    public static final String STATUS_CHANGED = "changed";
 
     public static String generate(String filepath1, String filepath2, String format) throws IOException {
-        Map<String, Object> map1 = Parser.parse(filepath1);
-        Map<String, Object> map2 = Parser.parse(filepath2);
-        List<Node> diffList = buildDiffList(map1, map2);
+        Map<String, Object> map1 = Parser.parse(readData(filepath1), readExt(filepath1));
+        Map<String, Object> map2 = Parser.parse(readData(filepath2), readExt(filepath2));
+        List<Node> diffList = TreeDiffer.buildDiffList(map1, map2);
 
         return Formatter.format(diffList, format);
     }
@@ -27,27 +23,22 @@ public class Differ {
         return generate(filepath1, filepath2, Formatter.STYLISH);
     }
 
-    private static List<Node> buildDiffList(Map<String, Object> map1, Map<String, Object> map2) {
-        Set<String> allKeysSet = new TreeSet<>();
-        allKeysSet.addAll(map1.keySet());
-        allKeysSet.addAll(map2.keySet());
-        List<Node> diffList = new ArrayList<>();
+    private static String readData(String filepath) throws IOException {
+        Path path = Paths.get(filepath).normalize().toAbsolutePath();
 
-        for (String key : allKeysSet) {
-            String status;
-            Object oldValue = map1.get(key);
-            Object newValue = map2.get(key);
-            if (!map2.containsKey(key)) {
-                status = STATUS_REMOVED;
-            } else if (!map1.containsKey(key)) {
-                status = STATUS_ADDED;
-            } else if (Objects.equals(oldValue, newValue)) {
-                status = STATUS_UNCHANGED;
-            } else {
-                status = STATUS_CHANGED;
-            }
-            diffList.add(new Node(key, oldValue, newValue, status));
+        if (!Files.exists(path)) {
+            throw new NoSuchFileException("File " + path + " does not exist");
         }
-        return diffList;
+
+        return Files.readString(path);
+    }
+
+    private static String readExt(String filepath) throws IOException {
+        String ext = filepath.substring(filepath.lastIndexOf(".") + 1);
+        if (ext.equals("-1")) {
+            throw new IOException("No file extension");
+        }
+
+        return ext;
     }
 }
