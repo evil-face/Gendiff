81a45d8049a1f79251c0b26356381a56674a2283
evil_face <den.bragin@gmail.com>
2023-03-14 15:52:29 +0600

    refactor Formatter.java with a simpler logic

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -4,17 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.Node;
 import java.io.IOException;
 import java.util.List;
-import java.util.StringJoiner;
 
 public class Json {
-    public static String generate(List<Node> diffList) throws IOException {
+    public static String format(List<Node> diffList) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        StringJoiner sj = new StringJoiner(",", "[", "]");
-
-        for (Node node : diffList) {
-            sj.add(mapper.writeValueAsString(node));
-        }
-
-        return sj.toString();
+        return mapper.writeValueAsString(diffList);
     }
 }
