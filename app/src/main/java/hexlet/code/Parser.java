244fcd2fa97ee752c2f9006a7d4604d12052d42c
evil_face <den.bragin@gmail.com>
2023-03-14 15:53:01 +0600

    refactor Parser.java, extracted methods to Differ.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,12 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,34 +14,17 @@ public class Parser {
     public static final String YAML_EXT_1 = "yml";
     public static final String YAML_EXT_2 = "yaml";
 
-    public static Map<String, Object> parse(String filepath) throws IOException {
-        Path path = Paths.get(filepath).normalize().toAbsolutePath();
-        if (!Files.exists(path)) {
-            throw new NoSuchFileException("File " + path + " does not exist");
-        }
+    public static Map<String, Object> parse(String content, String ext) throws IOException {
+        Map<String, Object> parsedMap = createMapper(ext).readValue(content, new TypeReference<>() { });
 
-        String ext = filepath.substring(filepath.lastIndexOf(".") + 1);
-        if (ext.equals("-1")) {
-            throw new IOException("No file extension");
-        }
-
-        Map<String, Object> parsedMap = createMapper(ext)
-                .readValue(new File(path.toString()), new TypeReference<>() { });
-        if (parsedMap == null) {
-            return new HashMap<>();
-        }
-        return parsedMap;
+        return parsedMap == null ? new HashMap<>() : parsedMap;
     }
 
     private static ObjectMapper createMapper(String ext) {
-        ObjectMapper mapper;
-        if (ext.equalsIgnoreCase(JSON_EXT)) {
-            mapper = new ObjectMapper();
-        } else if (ext.equalsIgnoreCase(YAML_EXT_1) || ext.equalsIgnoreCase(YAML_EXT_2)) {
-            mapper = new YAMLMapper();
-        } else {
-            throw new RuntimeException("Not supported extension");
-        }
-        return mapper;
+        return switch (ext.toLowerCase()) {
+            case JSON_EXT -> new ObjectMapper();
+            case YAML_EXT_1, YAML_EXT_2 -> new YAMLMapper();
+            default -> throw new RuntimeException("Not supported extension");
+        };
     }
 }
