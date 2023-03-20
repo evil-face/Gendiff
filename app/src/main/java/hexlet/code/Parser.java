d1ad047105d3a770c8cb1e0e759cb452e133dc8b
evil_face <den.bragin@gmail.com>
2023-03-20 16:41:22 +0600

    refactor Parser.java, extracted methods for different extension parsing

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,8 +2,7 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,16 +14,27 @@ public class Parser {
     public static final String YAML_EXT_2 = "yaml";
 
     public static Map<String, Object> parse(String content, String ext) throws IOException {
-        Map<String, Object> parsedMap = createMapper(ext).readValue(content, new TypeReference<>() { });
-
-        return parsedMap == null ? new HashMap<>() : parsedMap;
-    }
-
-    private static ObjectMapper createMapper(String ext) {
-        return switch (ext.toLowerCase()) {
-            case JSON_EXT -> new ObjectMapper();
-            case YAML_EXT_1, YAML_EXT_2 -> new YAMLMapper();
+        switch (ext.toLowerCase()) {
+            case JSON_EXT -> {
+                return parseJson(content) == null ? new HashMap<>() : parseJson(content);
+            }
+            case YAML_EXT_1, YAML_EXT_2 -> {
+                return parseYaml(content) == null ? new HashMap<>() : parseYaml(content);
+            }
             default -> throw new RuntimeException("Not supported extension");
-        };
+        }
+
+        // return parsedMap == null ? new HashMap<>() : parsedMap;
     }
+
+    private static Map<String, Object> parseYaml(String content) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(content, new TypeReference<>() { });
+    }
+
+    private static Map<String, Object> parseJson(String content) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, new TypeReference<>() { });
+    }
+
 }
